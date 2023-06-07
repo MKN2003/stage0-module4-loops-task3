02804894416396920d9a70af4d5643f2a4eca505
MKN2003 <kazimovmiraziz197@gmail.com>
2023-06-07 18:21:07 +0500

    Changes have been made

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int t) {
+        int sum = 0;
+        String numString = Integer.toString(t);
 
+        for (int i = 0; i < numString.length(); i++) {
+            sum += Character.getNumericValue(numString.charAt(i));
+        }
+        System.out.println(sum);
     }
 }
